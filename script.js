7f94cee0ce44d22cd8752e9d93d86fad674d2e93
keanestack <74552264+keane-stack@users.noreply.github.com>
2021-09-22 11:53:44 +0000

    vogel is een ding, helaas met shitcode

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-
-class Vogel{
-constructor(x, y, w, h, vx, vy,_color){
-this.x = x;
-this.y = y;
-this.width = w;
-this.height = h;
-this.vx = vx;
-this.vy = vy;
-this.color = _color
-}
-
-draw() {
-fill(this.color)
-ellipse (this.x,this.y,50,50);
-this.x = this.x + this.vx;
-this.y = this.y + this.vy;
-}
-}
-
-
-
+var x
+var y
+var z //zwaartekracht
+var v //velocity
 function setup() {
-	createCanvas(500, 400);
+createCanvas(500, 500);
+x = 250;
+y = 250;
+
 }
-function draw() {
-background(254);
-ballList.forEach(function(ball){
-ball.draw();
-});
+
+function draw(){
+  	background(225);
+ ellipse(x,y,50,50);
+  x = x + vx;
+  y = y + vy;
+
+
+
+  if(x <= 0 || x >= 500){
+    vx = vx * -1;
+  }
+
+  if(y <= 0 || y >= 500){ 
+    vy = vy * -1;
+  }
+
 }
+
+
+
+
