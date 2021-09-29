dea86674009427316cb56c134dc31cc69230d20f
keanestack <74552264+keane-stack@users.noreply.github.com>
2021-09-29 12:04:01 +0000

    velocity aangepast en eventjes een checkpoint

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-var y; 
-var accel; 
-var velocity; 
-var mass; 
+var y;
+var accel;
+var velocity;
+var mass;
 var jumper;
 
 
 function setup() {
   createCanvas(640, 360);
-  
-  y = 0;  
-  velocity = 0; 
-  mass = 30; 
-  
-  accel = mass * 0.1; 
+
+  y = 0;
+  velocity = 0;
+  mass = 30;
+
+  accel = mass * 0.1;
 }
 
 function draw() {
   background(127);
-  fill(255,0,0);
-  
-  velocity += accel; 
+  fill(255, 0, 0);
+
+  velocity += accel;
   y += velocity;
-	ellipse(width/2,y, mass,mass); 
-  
-  if (y > height - mass/2) {
+  ellipse(width / 2, y, mass, mass);
+
+  if (y > height - mass / 2) {
     // A little dampening when hitting the bottom
     velocity *= -0.6;
-    y = height - mass/2;
+    y = height - mass / 2;
   }
 }
 
 function keyPressed() {
-    velocity = -50;
+  velocity = -20;
 }
 
 
