f296a3a0ac421ff74304358b870fdfbe0d3a04a3
bartrl <74553307+Bart-tabak@users.noreply.github.com>
2021-10-11 17:45:41 +0000

    één bewegende buis loop gemaakt.

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-var accel;
-var velocity;
-var mass;
-var jumper;
-
 var bird;
+var buizen;
 
-
-class Pipe {
+class Buis {
   constructor(x, y, h) {
     this.x = x;
     this.y = y;
     this.h = h;
+    this.w = 30;
   }
 
   draw() {
-    
+    rect(this.x, this.y, this.w, this.h);
+    this.x -= 4;
   }
 }
 
@@ -44,19 +41,30 @@ function setup() {
   createCanvas(640, 360);
 
   bird = new Bird();
+  buizen = [];
 }
 
 function draw() {
   background(127);
   fill(255, 0, 0);
 
+  // Voor elke zoveel frames, één buis.
+  if (frameCount % 80 == 0) {
+    buizen.push(new Buis(width, 100, 100));
+  }
+
   bird.draw();
+
+  // Gaat door elke buis in de buizen lijst.
+  buizen.forEach(buis =>
+  {
+    // Tekent deze buis.
+    buis.draw();
+  });
 }
 
 function keyPressed() {
   bird.velocity = -15;
 }
-
-
 
 
